c4d5addd9b7fe48b7ccf2ed113c4079a7dc6ab10
Jaqueline <jaquelinebr@gmail.com>
2022-01-12 01:38:30 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,8 +1,8 @@
-import MovieScore from 'components/MovieScore';
-import './styles.css';
+import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 function MovieCard() {
-
   const movie = {
     id: 1,
     image:
@@ -12,19 +12,23 @@ function MovieCard() {
     score: 4.5,
   };
 
-    return (
-      <div>
-        <img
-          className="dsmovie-movie-card-image"
-          src={movie.image}
-          alt={movie.title}
-        />
-        <div className="dsmovie-card-bottom-container">
-          <h3>{movie.title}</h3>
-          <MovieScore />
+  return (
+    <div>
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <MovieScore />
+
+        <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-        </div>
+        </Link>
+
       </div>
-    );
+    </div>
+  );
 }
 export default MovieCard;
